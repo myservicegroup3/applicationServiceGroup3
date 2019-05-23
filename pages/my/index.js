099d4f69e5707456eaa2f2da99b7c2c5da1d1f57
Zero-Qzy <Qi_zero@outlook.com>
2019-05-24 07:23:08 +0800

    add the code

---
+++ pages/my/index.js
@@ -0,0 +1,97 @@
+// pages/my/index.js
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nickname: "点击登录",
+    imgsrc: '../../image/doge.jpg',
+    out: '未登录',
+    dd: "",
+    cc: "login",
+    ff: "fff0"
+  },
+  quit:function(){
+    wx.navigateTo({
+      url: 'pagess/quit/quit',
+    })
+  },
+  demo: function () {
+    wx.navigateTo({
+      url: '../my/pagess/login/login'
+    })
+  },
+  demo1: function () {
+    wx.navigateTo({
+      url: '../third/pagess/login/login'
+    })
+  },
+  demo2: function () {
+    wx.navigateTo({
+      url: '../third/pagess/contact/contact'
+    })
+  },
+  demo3: function () {
+    wx.navigateTo({
+      url: '../third/pagess/setting/setting'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/what/index.js
@@ -0,0 +1,94 @@
+// pages/what/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    characteristicList: [{
+      text: "北食堂"
+    }, {
+      text: "南食堂"
+    }, {
+      text: "外卖"
+    }, 
+      {
+        text: "出去吃"
+      }, 
+      ],
+    characteristicList1: [{
+      text: "早餐"
+    }, {
+      text: "午餐"
+    }, {
+      text: "晚餐"
+      }, {
+        text: "夜宵"
+      },
+    ],
+    characteristicList2: [{
+      text: "北食堂"
+    }, {
+      text: "南食堂"
+    }, {
+      text: "外卖"
+    },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
