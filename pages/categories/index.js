7b743f4f5f4802b05c1d7cae5dc7c1b732f67430
hailstucky <251707549@qq.com>
2019-06-02 21:53:00 +0800

    updating

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,48 +1,13 @@
-// pages/main/index.js
+// pages/categories/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-     block1:'block1',
-     block3:'block3',
-     istrue:0,
-     dd:0
+
   },
 
-  tomenu: function () {
-    wx.navigateTo({
-      url: '../main/pagess/menu/menu'
-    })
-  },
-  biass:function(){
-      this.setData({
-        istrue: 1,
-      })
-  },
-  biass2: function () {
-    this.setData({
-      istrue: 0,
-    })
-  },
-  normal:function()
-  {
-    var that=this;
-    that.setData({
-      block1:'block1',
-      block3:'block3'
-    })
-    this.onLoad();
-  },
-  diet: function () {
-    var that = this;
-    that.setData({
-      block1: 'block1-2',
-      block3: 'block3-2'
-    })
-    this.onLoad();
-  },
   /**
    * 生命周期函数--监听页面加载
    */
